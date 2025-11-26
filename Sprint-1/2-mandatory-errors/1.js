a38ebfea3b7900549bb2a5a2018dcd231df08e62
jannahpitogo <pitogojannah@outlook.com>
2025-11-26 14:04:49 +0100

    Fixed the type of age variable and expression to satisfy the instruction. Added some comments to explain.

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,11 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
-age = age + 1;
+var age = 33;
+
+//I changed the type const because this means that any variable declared as const, it's value cannot be changed. 
+//I put var so that I can reassigned the value age to another value. 
+
+age =  1;
+
+//In the previous line I remove the age + expression from the variable because this just means that it will add 1 from the initial value of age. 
+//This will not satisfy the instruction.
