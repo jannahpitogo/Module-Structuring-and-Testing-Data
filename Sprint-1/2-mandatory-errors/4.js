1f55d0719f8c6fc7d49ac36c4608addf207b2920
jannahpitogo <pitogojannah@outlook.com>
2025-11-26 15:17:35 +0100

    Added some comments to explain the error as well as change the start of the variable names to remove the error.

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,5 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const twelveHourClockTime = "20:53";
+const twentyFourHourClockTime = "08:53";
+
+//This gives and error because a variable name cannot start with a number. 
+//It only can start with underscore and letter. 
