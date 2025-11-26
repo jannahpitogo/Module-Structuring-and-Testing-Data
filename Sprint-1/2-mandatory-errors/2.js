00646faf17dcc060af6adaa2fa2b8afb8b870408
jannahpitogo <pitogojannah@outlook.com>
2025-11-26 14:10:26 +0100

    Fixed the error by declaring first the value of cityofBirth before doing the print. Added some comments explaining the error

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,12 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+// console.log(`I was born in ${cityOfBirth}`);
+// const cityOfBirth = "Bolton";
+
+//You can't print because the value of the variable cityofBirth was declared after the print expression.
+//So it doesn't know what the value of cityofBirth. 
+//To fix this declared the value first then do the print.
+
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
