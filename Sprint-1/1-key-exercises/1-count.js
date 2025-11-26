c2da5c631ab6c7dd48df06ea539554b7785c792c
jannahpitogo <pitogojannah@outlook.com>
2025-11-26 12:54:38 +0100

    Add explanation comment for count increment operation

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -2,5 +2,7 @@ let count = 0;
 
 count = count + 1;
 
+//What the line 3 is doing is getting the initial/declared value of count and adding 1 to it and storing it also to the same variable count. 
+
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
