fd03386e18fe71b13da55c18d5dcf7658d9add9d
jannahpitogo <pitogojannah@outlook.com>
2025-11-28 16:02:09 +0100

    Fixed some errors

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,6 +1,6 @@
 // trying to create an age variable and then reassign the value by 1
 
-var age = 33;
+let age = 33;
 
 //I changed the type const because this means that any variable declared as const, it's value cannot be changed. 
 //I put var so that I can reassigned the value age to another value. 
