8efa18c76db40df84c3d3914d75d22fb05a9dc04
jannahpitogo <pitogojannah@outlook.com>
2025-12-04 13:00:18 +0100

    fixed and explain the code for 0.js

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,26 @@
 // Predict and explain first...
-//  =============> write your prediction here
+// What will happen is that it will capitalize the first letter of inputted string, 
+// and then it will get also the inputted string and slice (show) from the index 1 to the end. 
+// And stored it to the str (same variable)
+//and return str
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
+// function capitalise(str) {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
+
+// capitalise("hello world!");
+
+// The error is due to the "str" already declared, when you call a function, javascript is making
+// a new declaration with the value you inputted. So declaring it again inside will declare it again causing an error. 
+
+
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  console.log(str);
 }
 
-// =============> write your explanation here
-// =============> write your new code here
+capitalise("hello world!");
