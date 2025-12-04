de022c0a24a0f10f26eff38eb6efffdd75282ab2
jannahpitogo <pitogojannah@outlook.com>
2025-12-04 14:16:55 +0100

    Fixed and explained the code in 1.js

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,19 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  the function will return nothing, since it was already ended by the semicolon where the next expression
+// will do nothing since the function already returned something which is undefined. 
+//  For the console.log outside it will show "The sum of 10 and 32 is undefined" since the function returned undefined. 
+// function sum(a, b) {
+//   return;
+//   a + b;
+// }
+
+// console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// It does not return an error but logged the string but inside the function call it returned undefined. 
+// Finally, correct the code to fix the problem
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
