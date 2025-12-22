920020c75ba68e21c05a93ad2d5099cf3707464c
jannahpitogo <pitogojannah@outlook.com>
2025-12-22 12:13:04 +0100

    added test cases for 3-get-card-value.test.js

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -1,8 +1,14 @@
 const getCardValue = require("./3-get-card-value");
 
 test("should return 11 for Ace of Spades", () => {
-    const aceofSpades = getCardValue("A♠");
-    expect(aceofSpades).toEqual(11);
+    expect(getCardValue("A♠")).toEqual(11);
+    expect(getCardValue("2♠")).toEqual(2);
+    expect(getCardValue("5♠")).toEqual(5);
+    expect(getCardValue("J♠")).toEqual(10);
+    expect(getCardValue("Q♠")).toEqual(10);
+    expect(getCardValue("K♠")).toEqual(10);
+    expect(getCardValue("10♠")).toEqual(10);
+    expect(getCardValue("1234789")).toEqual("Invalid card rank");
     });
 
 // Case 2: Handle Number Cards (2-10):
